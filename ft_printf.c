cb7e4ff9b9a2c820c7a29abb5c8424aea087b3da
barbaroliveira <barbara.pocas@gmail.com>
2023-06-06 17:55:52 +0100

    change

--- ft_printf.c
+++ ft_printf.c
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "printf.h"
 
+/*criar regra para %c - return 1 char*/
+//ft_putchar
+
+/*criar regra para %s - return n char*/
+/*criar regra para %d - return de um pointer em formato decimal*/
+/*criar regra para %i - return de um integer na base 10*/
+//ft_putnbr
+
+/*criar regra para %x - return de um numero em hexadecimal formato lowercase*/
+/*criar regra para %X - return de um numero em hexadecimal formato uppercase*/
+//chamar putnbr_base e fazer if para transformar em lower e upper
+
+/*criar regra para %u - return de um unsigned decimal na base 10*/
+//nova?
+
+/*criar regra para %p - return de um pointer em formato hexadecimal*/
+//putnbr_base??
+
+/*criar regra para %% - return de um sinal de %*/
+
+/*criar funcao principal que vai chamar as funcoes !=s
+*/
