abdda4b34908887da60f6d5858e01786b0d3766f
barbaroliveira <barbara.pocas@gmail.com>
2023-07-25 15:18:52 +0100

    null in %s

--- ft_printf.c
+++ ft_printf.c
@@ -23,6 +23,11 @@ int	ft_printstr(char *str)
 	int	i;
 
 	i = 0;
+	if(!str)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
 	while (str[i])
 	{
 		write(1, &str[i], 1);
--- ft_printf.h
+++ ft_printf.h
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <stdarg.h>
+# include "libft/libft.h"
 
 /*_________[FUNCTIONS]____________*/
 int	ft_putchar(char c);
